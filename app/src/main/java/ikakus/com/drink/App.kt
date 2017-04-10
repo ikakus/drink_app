a0498422e1135479aa362fa0f30f9fdbc951cca7
ikakus <idadianidev@gmail.com>
2017-04-10 10:32:04 +0400

    water level with accelerometer

--- app/src/main/java/ikakus/com/drink/App.kt
+++ app/src/main/java/ikakus/com/drink/App.kt
@@ -2,6 +2,7 @@ package ikakus.com.drink
 
 import android.app.Application
 import android.content.Context
+import com.github.nisrulz.sensey.Sensey
 
 /**
  * Created by ikakus on 4/4/17.
@@ -10,6 +11,7 @@ class App  : Application() {
     override fun onCreate() {
         super.onCreate()
         instance.mContext = this
+        Sensey.getInstance().init(this, Sensey.SAMPLING_PERIOD_UI)
     }
 
     object instance{
--- app/src/main/java/ikakus/com/drink/waterview/WaterLevel.kt
+++ app/src/main/java/ikakus/com/drink/waterview/WaterLevel.kt
@@ -8,9 +8,13 @@ import android.view.View
 import android.view.ViewTreeObserver
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.FrameLayout
+import com.github.nisrulz.sensey.RotationAngleDetector
+import com.github.nisrulz.sensey.Sensey
 import ikakus.com.drink.R
 import ikakus.com.drink.wave.WaveHelper
 import ikakus.com.drink.wave.WaveView
+import kotlinx.android.synthetic.main.view_water_level.view.*
+
 
 /**
  * Created by ikakus on 4/4/17.
@@ -20,6 +24,12 @@ class WaterLevel : FrameLayout {
     val mBorderColor = Color.parseColor("#3F51B5")
     val mBorderWidth = 0
     val mDefaultPercentage = 10
+    private var view: View? = null
+    private var mContext: Context? = null
+    private var waveView: WaveView? = null
+
+    var currentAngle: Float = 0f
+
     constructor(context: Context) : super(context) {
         initView(context)
     }
@@ -32,24 +42,23 @@ class WaterLevel : FrameLayout {
         initView(context)
     }
 
-
-    private var view: View? = null
-
-    private var mContext: Context? = null
-
     private fun initView(context: Context) {
         mContext = context
         view = LayoutInflater.from(context).inflate(R.layout.view_water_level, this, true)
-        var waveView: WaveView = (view as View?)!!.findViewById(R.id.wave_view) as WaveView
-        waveView.setBorder(mBorderWidth, mBorderColor)
-        waveView.setShapeType(WaveView.ShapeType.SQUARE)
-        var waveHelper = WaveHelper(waveView)
+        waveView = (view as View?)!!.findViewById(R.id.wave_view) as WaveView
+        waveView?.setBorder(mBorderWidth, mBorderColor)
+        waveView?.setShapeType(WaveView.ShapeType.SQUARE)
+        val waveHelper = WaveHelper(waveView!!)
         waveHelper.start()
+
+        Sensey.getInstance().startRotationAngleDetection(rotationAngleListener)
 
         afterMeasured {
             setDefaultPositions(mDefaultPercentage)
         }
     }
+
+    private var biggerWidth: Int = 0
 
     private fun setDefaultPositions(percent: Int) {
         val percentHeight = height / 100
@@ -58,41 +67,47 @@ class WaterLevel : FrameLayout {
         val bottomBack = view?.findViewById(R.id.bottom_back_color)
         val waveView = view?.findViewById(R.id.wave_view)
 
-        val botParams  = bottomBack!!.layoutParams
-        botParams.height = height
-        (botParams as FrameLayout.LayoutParams).setMargins(0, newHeight , 0 ,0)
+        biggerWidth = height * 2
+
+        val botParams = bottomBack!!.layoutParams
+        botParams.height = biggerWidth
+        botParams.width = biggerWidth
+        var leftMargin = -500
+
+
+        (botParams as FrameLayout.LayoutParams).setMargins(leftMargin, newHeight, 0, 0)
         bottomBack.layoutParams = botParams
 
-        val waveParams = waveView?.layoutParams
-        (waveParams as FrameLayout.LayoutParams).setMargins(0, newHeight , 0 ,0)
+        val waveParams = waveView!!.layoutParams
+        waveParams.width = height
+        (waveParams as FrameLayout.LayoutParams).setMargins(leftMargin, newHeight, 0, 0)
         waveView.layoutParams = waveParams
 
     }
+
+    private var newHeight: Int = 0
 
     fun setPercentageWithAnimation(percent: Int) {
         var mAnimationDuration = mContext?.getResources()?.getInteger(android.R.integer.config_mediumAnimTime)
 
         val percentHeight = height / 100
-        var newHeight = (height - percentHeight * percent)
+        newHeight = (height - percentHeight * percent)
         // show waves a little bit when percent is 0
-        if(percent == 0){
+        if (percent == 0) {
             newHeight = (height - percentHeight * 5)
         }
 
-        val bottomBack = view?.findViewById(R.id.bottom_back_color)
-        val waveView = view?.findViewById(R.id.wave_view)
-
-        var mWavesAnimation = waveView!!.animate()?.setDuration(mAnimationDuration!!.toLong())
+        var mWavesAnimation = wave_view!!.animate()?.setDuration(mAnimationDuration!!.toLong())
         mWavesAnimation!!.interpolator = AccelerateDecelerateInterpolator()
         mWavesAnimation!!.y(newHeight.toFloat())
 
-        var mTopBackAnimation = bottomBack!!.animate()?.setDuration(mAnimationDuration!!.toLong())
+        var mTopBackAnimation = bottom_back_color!!.animate()?.setDuration(mAnimationDuration!!.toLong())
         mTopBackAnimation!!.interpolator = AccelerateDecelerateInterpolator()
-        mTopBackAnimation!!.y(newHeight.toFloat())
+        mTopBackAnimation!!.y(newHeight.toFloat() + 10)
 
     }
 
-    inline fun <T: View> T.afterMeasured(crossinline f: T.() -> Unit) {
+    inline fun <T : View> T.afterMeasured(crossinline f: T.() -> Unit) {
         viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
             override fun onGlobalLayout() {
                 if (measuredWidth > 0 && measuredHeight > 0) {
@@ -102,5 +117,30 @@ class WaterLevel : FrameLayout {
             }
         })
     }
+
+    private val threshold: Float = 2f
+    private val maxAngle: Float = 20f
+    var rotationAngleListener: RotationAngleDetector.RotationAngleListener
+            = RotationAngleDetector.RotationAngleListener { angleInAxisX, angleInAxisY, angleInAxisZ ->
+        // Do something with the angles, values are in degrees
+        var angle = Math.round(angleInAxisZ * -1).toFloat()
+        if (Math.abs(currentAngle - Math.abs(angleInAxisZ)) > threshold) {
+
+            if (angle > maxAngle) {
+                angle = maxAngle
+            }
+
+            if (angle < -maxAngle) {
+                angle = -maxAngle
+            }
+            currentAngle = angle
+            waveView?.rotation = angle
+            bottom_back_color?.rotation = angle
+        }
+        val xPivot: Float = (biggerWidth / 4).toFloat()
+        bottom_back_color?.pivotX = xPivot
+        waveView?.pivotX = xPivot
+    }
+
 
 }
